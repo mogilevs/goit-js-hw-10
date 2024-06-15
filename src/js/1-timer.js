430652adf77038efe57a39b0e8290b3ff321b2e8
Oleh Mogylevsky <159718895+mogilevs@users.noreply.github.com>
2024-06-15 19:55:29 +0300

    fix vite

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,3 +1,8 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
