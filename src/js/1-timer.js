edd315b4a966ac555ade71cc5b0ead7c9d0e0553
Oleh Mogylevsky <159718895+mogilevs@users.noreply.github.com>
2024-06-16 00:06:24 +0300

    update draft1

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -24,7 +24,7 @@ function convertMs(ms) {
 
 let userSelectedDate;
 const btn = document.querySelector('button');
-btn.disabled = "true";
+btn.disabled = 'true';
 const spans = document.querySelectorAll('.value');
 const days = spans[0];
 const hours = spans[1];
@@ -38,12 +38,12 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     if (selectedDates[0] < Date.now()) {
-      alert("Please choose a date in the future");
-      btn.disabled = "true";
+      alert('Please choose a date in the future');
+      btn.disabled = 'true';
       return;
     } else {
       userSelectedDate = selectedDates[0];
-      btn.removeAttribute("disabled");
+      btn.removeAttribute('disabled');
     }
   },
 };
@@ -52,24 +52,27 @@ const fp = flatpickr(input, options);
 
 btn.addEventListener('click', startTimer);
 
-
-
 function startTimer() {
   const intervalId = setInterval(timer, 1000);
-  
+  function timer() {
+    let period = userSelectedDate - Date.now();
+    if (period > 0) {
+      let remainingTime = convertMs(period);
+      days.textContent = addZero(remainingTime.days);
+      hours.textContent = addZero(remainingTime.hours);
+      minutes.textContent = addZero(remainingTime.minutes);
+      seconds.textContent = addZero(remainingTime.seconds);
+      btn.disabled = 'true';
+      input.disabled = 'true';
+    } else {
+      clearInterval(intervalId);
+      btn.removeAttribute('disabled');
+      input.removeAttribute('disabled');
+    }
+  }
 }
 
-function timer() {
-  let period = userSelectedDate - Date.now();
-  let remainingTime = convertMs(period);
-  days.textContent = remainingTime.days;
-  hours.textContent = remainingTime.hours;
-  minutes.textContent = remainingTime.minutes;
-  seconds.textContent = remainingTime.seconds;
-  btn.disabled = "true";
-  input.disabled = "true";
-}
-
-function addLeadingZero(str, value) {
-  return str.padStart(value, '0');
+function addZero(value) {
+  const str = String(value);
+  return str.padStart(2, '0');
 }
