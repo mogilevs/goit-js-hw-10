e015ebaf73ae1e5994f683a01cf69f5d89f3e87d
Oleh Mogylevsky <159718895+mogilevs@users.noreply.github.com>
2024-06-15 21:48:27 +0300

    do draft task 1

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -22,7 +22,54 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+let userSelectedDate;
+const btn = document.querySelector('button');
+btn.disabled = "true";
+const spans = document.querySelectorAll('.value');
+const days = spans[0];
+const hours = spans[1];
+const minutes = spans[2];
+const seconds = spans[3];
 
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < Date.now()) {
+      alert("Please choose a date in the future");
+      btn.disabled = "true";
+      return;
+    } else {
+      userSelectedDate = selectedDates[0];
+      btn.removeAttribute("disabled");
+    }
+  },
+};
+const input = document.querySelector('#datetime-picker');
+const fp = flatpickr(input, options);
+
+btn.addEventListener('click', startTimer);
+
+
+
+function startTimer() {
+  const intervalId = setInterval(timer, 1000);
+  
+}
+
+function timer() {
+  let period = userSelectedDate - Date.now();
+  let remainingTime = convertMs(period);
+  days.textContent = remainingTime.days;
+  hours.textContent = remainingTime.hours;
+  minutes.textContent = remainingTime.minutes;
+  seconds.textContent = remainingTime.seconds;
+  btn.disabled = "true";
+  input.disabled = "true";
+}
+
+function addLeadingZero(str, value) {
+  return str.padStart(value, '0');
+}
