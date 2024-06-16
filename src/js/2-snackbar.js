db9db3075075ee2bc0cd321e61a395e0aae246e2
Oleh Mogylevsky <159718895+mogilevs@users.noreply.github.com>
2024-06-16 21:01:24 +0300

    do task2

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,2 +1,61 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', formHandler);
+
+function formHandler(evt) {
+  evt.preventDefault();
+  const delay = form.elements.delay.value;
+  const settle = form.elements.state.value;
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (settle === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+    
+  promise
+    .then(value => {
+      iziToast.show({
+        position: 'topCenter',
+        icon: 'icon-svg',
+        iconUrl: '../img/ok.png',
+        iconColor: 'white',
+        title: 'OK',
+        titleColor: 'white',
+        backgroundColor: 'green',
+        messageColor: 'white',
+        message: `Fulfilled promise in ${value}ms`,
+      });
+    })
+    .catch(error => {
+      iziToast.show({
+        position: 'topCenter',
+        icon: 'icon-svg',
+        iconUrl: '../img/err.png',
+        iconColor: 'white',
+        title: 'Error',
+        titleColor: 'white',
+        backgroundColor: 'red',
+        messageColor: 'white',
+        message: `Rejected promise in ${error}ms`,
+      });
+    });
+  form.reset();
+}
+
+const inputs = document.querySelectorAll("input[type='radio']");
+inputs.forEach(input => {
+  input.addEventListener('change', function() {
+    const fieldset = this.closest('fieldset');
+    if (this.checked) {
+      fieldset.style.borderColor = '#000';
+    } else {
+      fieldset.style.borderColor = '#808080';
+    }
+  });
+});
